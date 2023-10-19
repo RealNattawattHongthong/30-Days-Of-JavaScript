b8b2326120c005a5345b34faf09b3f1a5202165e
Nattawatt Hongthong <73651838+RealNattawattHongthong@users.noreply.github.com>
2023-10-19 12:55:07 +0000

    first commit need review

---
+++ Thai/01_Day_Introduction/01_day_starter/variable.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = 'Asabeneh'    // ชื่อ
+let lastName = 'Yetayeh'      // นามสกุล
+let country = 'Finland'       // country
+let city = 'Helsinki'         // capital city
+let age = 100                 // age in years
+let isMarried = true
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Asabeneh',     //name of a person
+  job = 'teacher',
+  live = 'Finland'
